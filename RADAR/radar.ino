7d1efbf37e1cf0ca906eb2b30e5c0c74e86262a2
S Ragavendar <ragajks@gmail.com>
2020-09-07 20:40:02 +0530

    Update radar.ino

--- RADAR/radar.ino
+++ RADAR/radar.ino
@@ -1,12 +1,6 @@
-
- 
-// Incluímos la librería para poder controlar el servo
 #include <Servo.h>
- 
-// Declaramos la variable para controlar el servo
 Servo servoMotor;
 
-// Definiendo pines
 const int trigPin = 3;
 const int echoPin = 5;
 const int servoPin = 9;
@@ -14,19 +8,14 @@ const int servoPin = 9;
 int const count = 7;
 int posiciones[count] = {0,30,60,90,120,150,180};
 
-// varialbes
 long duration;
 int distance;
-int timeDelay =700; // tiempo de espera de servo para calcular la distancia
- 
+int timeDelay =700;
 void setup() {
-  // Iniciamos el monitor serie para mostrar el resultado
   Serial.begin(9600);
- 
-  // Iniciamos el servo para que empiece a trabajar con el pin 9
   servoMotor.attach(servoPin);
-  pinMode(trigPin, OUTPUT); // Setear trigPin como Salida
-  pinMode(echoPin, INPUT); // Setear echoPin como Entrada
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT); 
   
 }
  
@@ -35,7 +24,7 @@ void loop() {
   int tmpdist;
   for (i = 0; i < count; i = i + 1) {
     servoMotor.write(posiciones[i]);
-    delay(timeDelay); // esperar a que se mueva el servo
+    delay(timeDelay);
     tmpdist = calcDist();
     Serial.print(posiciones[i]);
     Serial.print(";");
@@ -44,10 +33,8 @@ void loop() {
 }
 
 int calcDist() {
-  // Limpiar el trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // encender el trigPinpor 10 micro segundos
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
